e19000d0eb81c12d6c23647f94baa817eab1f1de
mr-akiio <91367974+mr-akiio@users.noreply.github.com>
2023-11-09 16:03:46 +0100

    Added readability

--- peak_alignment.py
+++ peak_alignment.py
@@ -49,8 +49,6 @@ with trsfile.open(sys.argv[1], 'r') as traces:
     lengthData = traces.get_headers().get(trsfile.Header.LENGTH_DATA)
     coding = traces.get_headers().get(trsfile.Header.SAMPLE_CODING)
 
-    #header = traces.get_headers()
-    #header[trsfile.Header.NUMBER_SAMPLES] = 1
     nameTrace = sys.argv[1][0:-4]+'+SAVE('+str(start)+','+str(number)+')+repaired.trs'
     with trsfile.trs_open(
         nameTrace,                 # File name of the trace set
@@ -61,11 +59,7 @@ with trsfile.open(sys.argv[1], 'r') as traces:
             trsfile.Header.TRS_VERSION: 2,
             trsfile.Header.SCALE_X: scale_X,
             trsfile.Header.SCALE_Y: scale_Y,
-            trsfile.Header.DESCRIPTION: 'Copied',
-            #Header.TITLE_SPACE = 255
-            #Header.LENGTH_DATA = 63
-            #trsfile.Header.TRACE_PARAMETER_DEFINITIONS: TraceParameterDefinitionMap(
-            #    {'LEGACY_DATA': TraceParameterDefinition(ParameterType.BYTE, 16, 0)})
+            trsfile.Header.DESCRIPTION: 'Copied'
         },
         padding_mode = trsfile.TracePadding.AUTO,# Optional: padding mode (defaults to TracePadding.AUTO)
         live_update = False              # Optional: updates the TRS file for live preview (small performance hit)
@@ -76,19 +70,14 @@ with trsfile.open(sys.argv[1], 'r') as traces:
     ) as wrtraces:
         master_trace = traces[0]
         for i in range(number):
-            print(f"Trace {i}")
-            if ((i) % 100000)==0:
-                print("here:",i)
+            print(f"Processing trace {i}")
+
             trace = traces[i]
-            #print(trace)
             trace_array = trace.samples[zoomS:zoomE]
             data = trace.parameters['LEGACY_DATA'].value
-            #coeffs = trace.parameters['COEFFICIENTS'].value
-            #print(coeffs)
-            #print("here",trace_array,data)
             trace_array = trace.samples[zoomS:zoomE]
-            # generate some integers
             rand_trace_array = np.zeros(trace_array.size,dtype=np.int8)
+
             for j in range(2, trace_array.size, 5):
                 a = int(statistics.mean(trace_array[j-2:j+3]))
 
@@ -103,12 +92,14 @@ with trsfile.open(sys.argv[1], 'r') as traces:
             else:
                 shift = max_master - find_max(rand_trace_array)
                 rand_trace_array = np.zeros(trace_array.size,dtype=np.int8)
+                
                 if shift > 0:
                     for i in range(trace_array.size-shift):
                         rand_trace_array[i+shift] = trace_array[i]
                 else:
                     for i in range(trace_array.size+shift):
                         rand_trace_array[i] = trace_array[i-shift]
+                        
             # Adding one Trace
             wrtraces.append(
                 trsfile.Trace(
