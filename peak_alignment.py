452a3e538ab6192036f1c1f51d5b358a3fdf6322
Michal Bahna <91367974+mr-akiio@users.noreply.github.com>
2023-11-09 17:54:13 +0100

    Find_min, created compressed graph + improved readability

--- peak_alignment.py
+++ peak_alignment.py
@@ -1,36 +1,24 @@
+import sys
+from time import time
+import statistics
 import trsfile
 from trsfile.parametermap import TraceSetParameterMap
-import trsfile.traceparameter as tp
-from trsfile.parametermap import TraceParameterMap, TraceParameterDefinitionMap
-from trsfile.traceparameter import StringParameter, ByteArrayParameter, ParameterType, TraceParameterDefinition
-import matplotlib.pyplot as plt
-import sys
+from trsfile.parametermap import TraceParameterMap
+from trsfile.traceparameter import ByteArrayParameter
 import numpy as np
-# import random, os
-import math
-import statistics
-
-import sys
 
 by_group = 5
 
 
-def find_max(trace):
-    max_val = trace[0]
-    max_index = 0
-    count = 0
+def find_min(trace_array_input):
+    min_val = trace_array_input[0]
+    min_index = 0
 
-    for i in range(by_group // 2, trace.size, by_group):
-        if trace[i] > max_val:
-            max_val = trace[i]
-            max_index = i
-            count = 0
-        else:
-            count += 1
-            if count == 20:
-                break
-    return max_index
-
+    for i in range(by_group // 2, trace_array_input.size // 2, by_group):
+        if trace_array_input[i] < min_val:
+            min_val = trace_array_input[i]
+            min_index = i
+    return min_index
 
 parameters = TraceSetParameterMap()
 print(parameters)
@@ -39,8 +27,10 @@ zoomS = 0
 zoomE = 220000
 
 start = 0
-number = 20
+number = 10
 displayLabels = 1
+
+start_time = time()
 
 with trsfile.open(sys.argv[1], 'r') as traces:
     print(traces.get_headers())
@@ -52,8 +42,6 @@ with trsfile.open(sys.argv[1], 'r') as traces:
     lengthData = traces.get_headers().get(trsfile.Header.LENGTH_DATA)
     coding = traces.get_headers().get(trsfile.Header.SAMPLE_CODING)
 
-    # header = traces.get_headers()
-    # header[trsfile.Header.NUMBER_SAMPLES] = 1
     nameTrace = sys.argv[1][0:-4] + \
         '+SAVE('+str(start)+','+str(number)+')+repaired.trs'
     with trsfile.trs_open(
@@ -67,10 +55,6 @@ with trsfile.open(sys.argv[1], 'r') as traces:
             trsfile.Header.SCALE_X: scale_X,
             trsfile.Header.SCALE_Y: scale_Y,
             trsfile.Header.DESCRIPTION: 'Copied',
-            # Header.TITLE_SPACE = 255
-            # Header.LENGTH_DATA = 63
-            # trsfile.Header.TRACE_PARAMETER_DEFINITIONS: TraceParameterDefinitionMap(
-            #    {'LEGACY_DATA': TraceParameterDefinition(ParameterType.BYTE, 16, 0)})
         },
         # Optional: padding mode (defaults to TracePadding.AUTO)
         padding_mode=trsfile.TracePadding.AUTO,
@@ -83,21 +67,16 @@ with trsfile.open(sys.argv[1], 'r') as traces:
     ) as wrtraces:
         master_trace = traces[0]
         for i in range(number):
-            print(f"Trace {i}")
-            if ((i) % 100000) == 0:
-                print("here:", i)
+            print(f"Processing trace N° {i}")
+
             trace = traces[i]
-            # print(trace)
-            trace_array = trace.samples[zoomS:zoomE]
             data = trace.parameters['LEGACY_DATA'].value
-            # coeffs = trace.parameters['COEFFICIENTS'].value
-            # print(coeffs)
-            # print("here",trace_array,data)
+
             trace_array = trace.samples[zoomS:zoomE]
-            # generate some integers
-            rand_trace_array = np.zeros(trace_array.size, dtype=np.int8)
+            rand_trace_array = np.zeros((trace_array.size), dtype=np.int8)
 
             for j in range(by_group // 2, trace_array.size, by_group):
+
                 low = by_group // 2
                 high = by_group // 2 + 1 if by_group % 2 == 1 else by_group // 2
                 a = int(statistics.mean(trace_array[j-low:j+high]))
@@ -111,24 +90,28 @@ with trsfile.open(sys.argv[1], 'r') as traces:
 
             if i == 0:
                 max_master = find_max(rand_trace_array)
+                rand_trace_array = np.zeros(trace_array.size, dtype=np.int8)
+
+                for i in range(trace_array.size):
+                    rand_trace_array[i] = trace_array[i]
             else:
                 shift = max_master - find_max(rand_trace_array)
                 rand_trace_array = np.zeros(trace_array.size, dtype=np.int8)
+
                 if shift > 0:
                     for i in range(trace_array.size-shift):
                         rand_trace_array[i+shift] = trace_array[i]
                 else:
                     for i in range(trace_array.size+shift):
                         rand_trace_array[i] = trace_array[i-shift]
+
             # Adding one Trace
             wrtraces.append(
                 trsfile.Trace(
                     coding,
                     rand_trace_array,
-                    TraceParameterMap({'LEGACY_DATA': ByteArrayParameter(data)  # ,
-                                       # 'COEFFICIENTS': StringParameter(coeffs)
-                                       })
+                    TraceParameterMap({'LEGACY_DATA': ByteArrayParameter(data)})
                 )
             )
-
-        print("done")
+        end_time = time()
+        print(f"done in {(end_time - start_time) // 60}m {((end_time - start_time) / 60) - ((end_time - start_time) // 60) }s")
