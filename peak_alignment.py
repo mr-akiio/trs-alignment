9e9741e00befd6cd41c298890f11661544e5c0a3
Matúš Renko <matus.renko@gmail.com>
2023-11-09 16:31:47 +0100

    Update peak_alignment.py

--- peak_alignment.py
+++ peak_alignment.py
@@ -6,18 +6,21 @@ from trsfile.traceparameter import StringParameter, ByteArrayParameter, Paramete
 import matplotlib.pyplot as plt
 import sys
 import numpy as np
-#import random, os
-import math 
+# import random, os
+import math
 import statistics
 
 import sys
+
+by_group = 5
+
 
 def find_max(trace):
     max_val = trace[0]
     max_index = 0
     count = 0
-    
-    for i in range(2, trace.size, 5):
+
+    for i in range(by_group // 2, trace.size, by_group):
         if trace[i] > max_val:
             max_val = trace[i]
             max_index = i
@@ -32,8 +35,8 @@ def find_max(trace):
 parameters = TraceSetParameterMap()
 print(parameters)
 
-zoomS=0
-zoomE=220000
+zoomS = 0
+zoomE = 220000
 
 start = 0
 number = 20
@@ -49,65 +52,82 @@ with trsfile.open(sys.argv[1], 'r') as traces:
     lengthData = traces.get_headers().get(trsfile.Header.LENGTH_DATA)
     coding = traces.get_headers().get(trsfile.Header.SAMPLE_CODING)
 
-    nameTrace = sys.argv[1][0:-4]+'+SAVE('+str(start)+','+str(number)+')+repaired.trs'
+    # header = traces.get_headers()
+    # header[trsfile.Header.NUMBER_SAMPLES] = 1
+    nameTrace = sys.argv[1][0:-4] + \
+        '+SAVE('+str(start)+','+str(number)+')+repaired.trs'
     with trsfile.trs_open(
         nameTrace,                 # File name of the trace set
         'w',                             # Mode: r, w, x, a (default to x)
         # Zero or more options can be passed (supported options depend on the storage engine)
-        engine = 'TrsEngine',            # Optional: how the trace set is stored (defaults to TrsEngine)
-        headers = {                      # Optional: headers (see Header class)
+        # Optional: how the trace set is stored (defaults to TrsEngine)
+        engine='TrsEngine',
+        headers={                      # Optional: headers (see Header class)
             trsfile.Header.TRS_VERSION: 2,
             trsfile.Header.SCALE_X: scale_X,
             trsfile.Header.SCALE_Y: scale_Y,
-            trsfile.Header.DESCRIPTION: 'Copied'
+            trsfile.Header.DESCRIPTION: 'Copied',
+            # Header.TITLE_SPACE = 255
+            # Header.LENGTH_DATA = 63
+            # trsfile.Header.TRACE_PARAMETER_DEFINITIONS: TraceParameterDefinitionMap(
+            #    {'LEGACY_DATA': TraceParameterDefinition(ParameterType.BYTE, 16, 0)})
         },
-        padding_mode = trsfile.TracePadding.AUTO,# Optional: padding mode (defaults to TracePadding.AUTO)
-        live_update = False              # Optional: updates the TRS file for live preview (small performance hit)
-                                         #   0 (False): Disabled (default)
-                                         #   1 (True) : TRS file updated after every trace
-                                         #   N        : TRS file is updated after N traces
+        # Optional: padding mode (defaults to TracePadding.AUTO)
+        padding_mode=trsfile.TracePadding.AUTO,
+        # Optional: updates the TRS file for live preview (small performance hit)
+        live_update=False
+        #   0 (False): Disabled (default)
+        #   1 (True) : TRS file updated after every trace
+        #   N        : TRS file is updated after N traces
 
     ) as wrtraces:
         master_trace = traces[0]
         for i in range(number):
-            print(f"Processing trace {i}")
-
+            print(f"Trace {i}")
+            if ((i) % 100000) == 0:
+                print("here:", i)
             trace = traces[i]
+            # print(trace)
             trace_array = trace.samples[zoomS:zoomE]
             data = trace.parameters['LEGACY_DATA'].value
+            # coeffs = trace.parameters['COEFFICIENTS'].value
+            # print(coeffs)
+            # print("here",trace_array,data)
             trace_array = trace.samples[zoomS:zoomE]
-            rand_trace_array = np.zeros(trace_array.size,dtype=np.int8)
+            # generate some integers
+            rand_trace_array = np.zeros(trace_array.size, dtype=np.int8)
 
-            for j in range(2, trace_array.size, 5):
-                a = int(statistics.mean(trace_array[j-2:j+3]))
+            for j in range(by_group // 2, trace_array.size, by_group):
+                low = by_group // 2
+                high = by_group // 2 + 1
+                a = int(statistics.mean(trace_array[j-low:j+high]))
 
-                rand_trace_array[j] = a
-                rand_trace_array[j+1] = a
-                rand_trace_array[j-1] = a
-                rand_trace_array[j-2] = a
-                rand_trace_array[j+2] = a
-                
+                for u in range(j - low, j + high):
+                    rand_trace_array[u] = a
+                # rand_trace_array[j+1] = a
+                # rand_trace_array[j-1] = a
+                # rand_trace_array[j-2] = a
+                # rand_trace_array[j+2] = a
+
             if i == 0:
                 max_master = find_max(rand_trace_array)
             else:
                 shift = max_master - find_max(rand_trace_array)
-                rand_trace_array = np.zeros(trace_array.size,dtype=np.int8)
-                
+                rand_trace_array = np.zeros(trace_array.size, dtype=np.int8)
                 if shift > 0:
                     for i in range(trace_array.size-shift):
                         rand_trace_array[i+shift] = trace_array[i]
                 else:
                     for i in range(trace_array.size+shift):
                         rand_trace_array[i] = trace_array[i-shift]
-                        
             # Adding one Trace
             wrtraces.append(
                 trsfile.Trace(
                     coding,
                     rand_trace_array,
-                    TraceParameterMap({'LEGACY_DATA': ByteArrayParameter(data)#,
-                                    #'COEFFICIENTS': StringParameter(coeffs)
-                                    })
+                    TraceParameterMap({'LEGACY_DATA': ByteArrayParameter(data)  # ,
+                                       # 'COEFFICIENTS': StringParameter(coeffs)
+                                       })
                 )
             )
 
